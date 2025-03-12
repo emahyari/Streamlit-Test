0691406e1da449c5aca18b29fa84d314d1df888a
Ehsan Mahyari <62964799+emahyari@users.noreply.github.com>
2025-03-12 11:31:55 -0400

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
+import matplotlib.pyplot as plt
 
 # Load the sales data
 @st.cache_data
@@ -30,7 +31,8 @@ st.subheader(f"Sales Data for {selected_country} in {selected_year}")
 st.write(filtered_df)
 
 # Line chart visualization
-fig = px.line(aggregated_sales, x="Date", y="Revenue", title="Sales Trend")
+# fig = px.line(aggregated_sales, x="Date", y="Revenue", title="Sales Trend")
+aggregated_sales.plot(figsize = (15, 5))
 st.plotly_chart(fig)
 
 st.sidebar.markdown("---")
